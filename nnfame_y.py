225fbc2fbcdb0abe09192156e81c268d50fb8ccc
leksandre <44774042+leksandre@users.noreply.github.com>
2024-01-15 17:50:55 +0500

    itZalupaWork

--- nnfame_y.py
+++ nnfame_y.py
@@ -35,13 +35,20 @@ global lastImg
 lastImg = ''
 global maxPerson
 maxPerson = 0
+global statObj
+statObj = 1
 global startPoint
 startPoint = time.time() 
 global PrevDetection
 PrevDetection = False
 global structable
 structable = False
-
+global listId
+listId = 0
+global newCols
+newCols = []
+global access_token
+access_token = ''
 
 
 execution_path = os.getcwd()
@@ -50,7 +57,7 @@ now = datetime.now()
 
 
 
-eachMin = 5
+eachMin = 1
 
 
 
@@ -62,6 +69,41 @@ eachMin = 5
 # os.environ ["CUDA_VISIBLE_DEVICES"] = ""
 
 
+#comments 
+
+
+    # for eachPrediction, eachProbability in zip(predictions, probabilities):
+    #   if(probabilities[0]>0.3):
+        # print(eachPrediction , " : " , eachProbability)
+        
+    # return detector.detectObjectsFromImage(
+    # input_image=os.path.join(execution_path , img1), # "вхдное" изображения для распознования
+    # output_image_path=os.path.join(execution_path , name1+now.strftime("%m_%d_%Y_%H_%M_%S")+".jpg"), # новое изображение которое является результатом обрабоки "входного изображения"
+    # minimum_percentage_probability=pp
+    # )
+    
+#get lists list
+# curl 'https://h1.h2/api/v8/lists?page=1&pageSize=100&appId=14' \
+#create stat list
+# curl 'https://h1.h2/api/v8/lists' \
+#   --data-raw 'AppId=14&name=statistic&description=stat+data+from+ours+camera&structure=%7B%22links%22%3A%5B%5D%2C%22tables%22%3A%5B%5D%7D' \
+# adding table
+# curl 'https://h1.h2/api/v8/lists/12/newtable' \
+#   --data-raw 'json=%5B%7B%22name%22%3A%22statofobjects%22%2C%22fields%22%3A%5B%5D%7D%5D' \
+# curl 'https://h1.h2/api/v8/lists/12/newtable' \
+# --data-raw 'json=%5B%7B%22name%22%3A%22statofobjects%22%2C%22fields%22%3A%5B%7B%22name%22%3A%22date%22%2C%22type%22%3A%22timestext%22%7D%2C%7B%22name%22%3A%22time%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22person%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22personAvg%22%2C%22type%22%3A%22text%22%7D%5D%7D%5D' \
+# new col
+#   curl 'https://h1.h2/api/v8/lists/12/newcolumn' \
+# --data-raw 'tableName=statofobjects&json=%5B%7B%22name%22%3A%22date%22%2C%22type%22%3A%22timestext%22%7D%5D' \
+# rem col
+# curl 'https://h1.h2/api/v8/lists/12/column?tableName=statofobjects&columnName=date' \
+# curl 'https://h1.h2/api/v8/lists/12/newtable' \
+# --data-raw 'json=%5B%7B%22name%22%3A%22statofobjects%22%2C%22fields%22%3A%5B%7B%22name%22%3A%22date%22%2C%22type%22%3A%22timestext%22%7D%2C%7B%22name%22%3A%22time%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22person%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22personAvg%22%2C%22type%22%3A%22text%22%7D%5D%7D%5D' \
+
+# add row
+# curl 'https://h1.h2/api/v8/list' \
+#   --data-raw 'listId=16&tableName=statofobjects&json=%7B%22date%22%3Anull%2C%22time%22%3A%22datetime1%22%2C%22person%22%3A%2212%22%2C%22personAvg%22%3A%220.8%22%7D' \
+#   --compressed
 
 # class adn func
 
@@ -71,24 +113,6 @@ class Response1:
         self.result = code1
         self.text = text1
 
-
-def fixStatistic():
-    pass
-
-def compare_detections(list1, list2, path=""):
-    # Проверяем, являются ли оба объекта списками
-    if isinstance(list1, list) and isinstance(list2, list):
-        # Проверяем, имеют ли списки одинаковую длину
-        if len(list1) != len(list2):
-            print(f"Различное количество элементов: {path}")
-        # Рекурсивно сравниваем каждый элемент списков
-        for i in range(min(len(list1), len(list2))):
-            compare_detections(list1[i], list2[i], f"{path}[{i}]")
-    else:
-        # Если объекты не являются списками, сравниваем их значения
-        if list1 != list2:
-            print(f"Различное значение: {path} - {list1}, {list2}")
-            
 def checkAndCreateColumn(nameCol):
     if not structable:
         print(f"'это не то': {structable}")
@@ -105,16 +129,80 @@ def checkAndCreateColumn(nameCol):
             colAvgExist = True
     Headers = { 'Authorization' : "Bearer "+str(access_token), 'Content-Type': 'application/x-www-form-urlencoded' }
     if not colAvgExist:
+        # listId = getListId() #get from global
         maindata = f"tableName=statofobjects&json=%5B%7B%22name%22%3A%{nameCol+'Avg'}%22%2C%22type%22%3A%22text%22%7D%5D"
-        r = simpleRequest(url=urlD+url_l, headers=Headers, data=maindata)
+        r = simpleRequest(url=urlD+url_l+'/'+str(listId)+'/newcolumn', headers=Headers, data=maindata)
     if not colNameExist:
+        # listId = getListId() #get from global
         maindata = f"tableName=statofobjects&json=%5B%7B%22name%22%3A%{nameCol}%22%2C%22type%22%3A%22text%22%7D%5D"
-        r = simpleRequest(url=urlD+url_l, headers=Headers, data=maindata)
+        r = simpleRequest(url=urlD+url_l+'/'+str(listId)+'/newcolumn', headers=Headers, data=maindata)
     getListId()
+    
+def fixStatistic(*params):
+    # print(f"fix stats {params}")
+    # print(f"fix stats params 0 {params[0]}")
+    # print(f"fix stats params 1 {params[1]}")
+    Headers = { 'Authorization' : "Bearer "+str(access_token), 'Content-Type': 'application/x-www-form-urlencoded' }
+    if 1:
+        # listId = getListId() #get from global
+        now_1 = datetime.now() 
+        time_1 = now_1.strftime("%H:%M:%S")
+        date_1 = now_1.strftime("%Y-%m-%d")
+        # date_1 = now_1.strftime("%d/%m/%Y %H:%M")
+        maindata = f"listId={listId}&tableName=statofobjects&json=%7B%22date%22%3A%22{date_1}%22%2C%22time%22%3A%22{time_1}%22%2C%22person%22%3A%22{params[0]}%22%2C%22personAvg%22%3A%22{params[1]}%22%7D"
+        print(f"fix stats maindata: {maindata}")
+        r = simpleRequest(url=urlD+(url_l[:-1]), headers=Headers, data=maindata, showresp = True)
+    pass
+
+def compare_detections(list1, list2, path=""):
+    if path=="": #если это "нулевой" уровень
+        stat1 = 0 
+    # Проверяем, являются ли оба объекта списками
+    if isinstance(list1, list) and isinstance(list2, list):
+        # Проверяем, имеют ли списки одинаковую длину
+        if len(list1) != len(list2):
+            print(f"Различное количество элементов: {path}")
+        # Рекурсивно сравниваем каждый элемент списков
+        for i in range(min(len(list1), len(list2))):
+            res = compare_detections(list1[i], list2[i], f"{path}[{i}]")
+            if path=="": #если это "нулевой" уроыень
+                if res:
+                    pass
+                else:
+                    stat1 = stat1+1
+            else:
+                return res
+    else:
+        # Если объекты не являются списками, сравниваем их значения
+        if list1 != list2:
+            print(f"Объект переместился: {path} - {list1}, {list2}")
+            return True
+        print(f"Объект не двигается: {path} - {list1}, {list2}")
+        return False
+    if path=="": #если это "нулевой" уровень
+        print(f"Объектов без движения: {stat1}")
+
+def compare_persons(list1, list2):
+    stat1 = 0
+    if isinstance(list1, list) and isinstance(list2, list):
+        if len(list1) != len(list2):
+            print(f"Различное количество элементов")
+        for i in range(len(list1)):
+            doNotMove = False
+            for j in range(len(list2)):
+              if list1[i]['box_points'] == list2[j]['box_points']:
+                  doNotMove = True
+                  print(f"!!!------Объект без движения: {list1[i]}")
+                  print(f"!!!------Объект без движения: {list2[j]}")
+            if doNotMove:
+                  stat1 = stat1+1
+    print(f"Объектов без движения: {stat1}")
+    return stat1
         
 def checkImg(pp=probaility,img1='',name1="image_1new"):
     
     global maxPerson
+    global statObj
     global startPoint
     global PrevDetection
     
@@ -127,67 +215,64 @@ def checkImg(pp=probaility,img1='',name1="image_1new"):
     execution_time = end_time - start_time  # вычисляем время выполнения
     print(f"- Время распознования: {execution_time} секунд")
     
-    personCount = 0
-    
-    CurDetection = detections
-    if PrevDetection:
-        compare_detections(PrevDetection,CurDetection)
+    global newCols
     
     for eachObject in detections:
         print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+        
+        print("--------------------------------")
+        if eachObject["name"]!='person':
+            if not eachObject["name"] in newCols:
+                checkAndCreateColumn(eachObject["name"])
+                newCols.append(eachObject["name"])
+    
+    
+  
+        
+    
+    
+    
+    # for eachObject in detections:
+    #     if eachObject["name"]=='person':
+    #         personCount = 1+personCount    
+            
+    CurDetection = detections
+    filtered_data_cur = [item for item in CurDetection if item['name'] == 'person']
+    personCount = len(filtered_data_cur)
+    
+    diffStat = 0
+    if PrevDetection:
+        filtered_data_prev = [item for item in PrevDetection if item['name'] == 'person']
+        diffStat = compare_persons(filtered_data_prev,filtered_data_cur)
+    
+    if diffStat>0:
+        statObj = statObj * (1 - (diffStat/personCount) )
 
-        if eachObject["name"]=='person':
-            personCount = 1+personCount
         # print(eachObject["box_points"][0] )
         # print(eachObject["box_points"][1] )
         # print(eachObject["box_points"][2] )
         # print(eachObject["box_points"][3] )
-        print("--------------------------------")
     
     
     if maxPerson<personCount:
         maxPerson=personCount
     
-    total_time = startPoint - start_time 
-    
+    total_time =  start_time - startPoint
+    print(f"maxPerson,statObj,total_time {maxPerson,statObj,total_time}")
     if total_time>(60*eachMin):
-        fixStatistic()
+        fixStatistic(maxPerson,statObj)
         startPoint = time.time() 
         maxPerson = 0
-    
+        statObj = 1
+    else:
+        time.sleep(5)
     
     PrevDetection = CurDetection
         
-    # for eachPrediction, eachProbability in zip(predictions, probabilities):
-    #   if(probabilities[0]>0.3):
-        # print(eachPrediction , " : " , eachProbability)
-        
-        
-    # return detector.detectObjectsFromImage(
-    # input_image=os.path.join(execution_path , img1), # "вхдное" изображения для распознования
-    # output_image_path=os.path.join(execution_path , name1+now.strftime("%m_%d_%Y_%H_%M_%S")+".jpg"), # новое изображение которое является результатом обрабоки "входного изображения"
-    # minimum_percentage_probability=pp
-    # )
-    
-#get lists list
-# curl 'https://domotel-admin.mobsted.ru/api/v8/lists?page=1&pageSize=100&appId=14' \
-#create stat list
-# curl 'https://domotel-admin.mobsted.ru/api/v8/lists' \
-#   --data-raw 'AppId=14&name=statistic&description=stat+data+from+ours+camera&structure=%7B%22links%22%3A%5B%5D%2C%22tables%22%3A%5B%5D%7D' \
-# adding table
-# curl 'https://domotel-admin.mobsted.ru/api/v8/lists/12/newtable' \
-#   --data-raw 'json=%5B%7B%22name%22%3A%22statofobjects%22%2C%22fields%22%3A%5B%5D%7D%5D' \
-# curl 'https://domotel-admin.mobsted.ru/api/v8/lists/12/newtable' \
-# --data-raw 'json=%5B%7B%22name%22%3A%22statofobjects%22%2C%22fields%22%3A%5B%7B%22name%22%3A%22date%22%2C%22type%22%3A%22timestamp+without+time+zone%22%7D%2C%7B%22name%22%3A%22time%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22person%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22personAvg%22%2C%22type%22%3A%22text%22%7D%5D%7D%5D' \
-# new col
-#   curl 'https://domotel-admin.mobsted.ru/api/v8/lists/12/newcolumn' \
-# --data-raw 'tableName=statofobjects&json=%5B%7B%22name%22%3A%22date%22%2C%22type%22%3A%22timestamp+without+time+zone%22%7D%5D' \
-# rem col
-# curl 'https://domotel-admin.mobsted.ru/api/v8/lists/12/column?tableName=statofobjects&columnName=date' \
-# curl 'https://domotel-admin.mobsted.ru/api/v8/lists/12/newtable' \
-# --data-raw 'json=%5B%7B%22name%22%3A%22statofobjects%22%2C%22fields%22%3A%5B%7B%22name%22%3A%22date%22%2C%22type%22%3A%22timestamp+without+time+zone%22%7D%2C%7B%22name%22%3A%22time%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22person%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22personAvg%22%2C%22type%22%3A%22text%22%7D%5D%7D%5D' \
 
-def simpleRequest(isGet = False, checkStruct = False, **params):
+
+
+def simpleRequest(isGet = False, checkStruct = False, showresp = False, **params):
         try:
             if isGet:
                 r = requests.get(**params)
@@ -214,10 +299,10 @@ def simpleRequest(isGet = False, checkStruct = False, **params):
             print(' over KeyError  ' + str(e))
             #return Response1("201", '')
             return False
-        # print('result text:'+str(r.text))
-        
-        # print('response:', (r))
-        # print('response type:', type(r))
+        if showresp:
+            print('result text:'+str(r.text))
+            print('response:', (r))
+            print('response type:', type(r))
         
         print('status_code:'+str(r.status_code))
         
@@ -249,7 +334,6 @@ def simpleRequest(isGet = False, checkStruct = False, **params):
         return r
 
 def getListId(tables=[]):
-    global access_token
     global structable
     Headers = { 'Authorization' : "Bearer "+str(access_token) }
     
@@ -259,9 +343,9 @@ def getListId(tables=[]):
     data = r.json()
 
     for dataList in data['data']:
-        print('dataList',dataList)
-        print('dataList name',dataList['attributes']['name'])    
-        print('dataList satatus',dataList['attributes']['status'])
+        # print('dataList',dataList)
+        # print('dataList name',dataList['attributes']['name'])    
+        # print('dataList satatus',dataList['attributes']['status'])
         if dataList['attributes']['name']=='statistic':
             listId = dataList['id']
             for table in dataList['attributes']['structure']['tables']:
@@ -272,6 +356,7 @@ def getListId(tables=[]):
                 
 def checkAndCreateList():
     global access_token
+    global listId
     
     PARAMS = {'login':log_e,'password':pass_e}
     url_e = urlD+(url_a.replace('userLogin555',log_e)).replace('userPassword888',pass_e)
@@ -306,7 +391,7 @@ def checkAndCreateList():
 
     if len(tables)==0:
         #createTable
-        maindata = 'json=%5B%7B%22name%22%3A%22statofobjects%22%2C%22fields%22%3A%5B%7B%22name%22%3A%22date%22%2C%22type%22%3A%22timestamp+without+time+zone%22%7D%2C%7B%22name%22%3A%22time%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22person%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22personAvg%22%2C%22type%22%3A%22text%22%7D%5D%7D%5D'
+        maindata = 'json=%5B%7B%22name%22%3A%22statofobjects%22%2C%22fields%22%3A%5B%7B%22name%22%3A%22date%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22time%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22person%22%2C%22type%22%3A%22text%22%7D%2C%7B%22name%22%3A%22personAvg%22%2C%22type%22%3A%22text%22%7D%5D%7D%5D'
         r = simpleRequest(url=urlD+url_l+'/'+str(listId)+'/newtable', headers=Headers, data=maindata)
     tables = []
     listId = getListId(tables)
